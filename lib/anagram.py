0cab8af97c7ca594d6f54a77d987fa3990cf7d6e
edith.githinji <edithgithinji2020@gmail.com>
2025-02-17 15:19:57 +0200

    anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        sorted_word = sorted(self.word)
+        matches = []
+
+
+        for anagram in possible_anagrams:
+            if sorted(anagram)== sorted_word:
+                matches.append(anagram)
+                
+        return matches
+      
+        
+        
